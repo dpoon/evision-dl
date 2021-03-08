3bb13a8ca5194aa06496dbdde32aa36b144f1a12
Dara Poon <dwpoon@mail.ubc.ca>
2021-03-08 15:36:50 -0800

    Version 1

--- download_evision_pdfs.py
+++ download_evision_pdfs.py
@@ -41,41 +41,63 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+# Include logger for debugging
+import logging
+logger = logging.getLogger()
+logging.basicConfig(  # Comment out logging.basicConfig when not debugging
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    filename="download_evision_pdfs.log"
+)
 # Path to the Firefox driver (geckodriver)
 os.environ['PATH'] = os.path.join(os.path.expanduser('~'), 'bin') + os.pathsep + os.environ['PATH']
 
 def download(dest_file, url, headers=[]):
+    logger.info("download(): SAVING DATA TO FILE {} FROM URL {} WITH HEADERS {}"
+                .format(dest_file, url, headers))
     if not os.path.isdir(os.path.dirname(dest_file)):
+        logger.info("download(): CREATING DIRECTORY FOR dest_file")
         os.makedirs(os.path.dirname(dest_file))
     with open(dest_file, 'wb') as f:
+        logger.info("download(): WRITING TO dest_file {} ...".format(dest_file))
         if not url:
             print(u'Nothing to download to {1}!'.format(url, dest_file))
+            logger.info("download(): NOTHING TO DOWNLOAD")
             return
         print(u'Downloading {0} to {1}...'.format(url, dest_file), end=None)
         opener = build_opener()
         opener.addheaders = headers
+        logger.info("download(): opener = {}".format(opener))
         res = opener.open(url)  # Python 3 can do with opener.open(url) as res:
         try:
             f.write(res.read())
             print(u' Done.')
         finally:
             res.close()
+            logger.info("download(): FINISHED DOWNLOAD")
+
 def click(driver, *expectation):
+    logger.info("click(): ATTEMPTING {} AT URL {}".format(expectation, driver.current_url))
     while True:
         try:
             return WebDriverWait(driver, 90).until(
                 EC.presence_of_element_located(expectation)
             ).click()
+        except TimeoutException:
+            logger.error("click(): TIMEOUT EXCEPTION")
         except ElementClickInterceptedException:
+            logger.warning("click(): CLICK INTERCEPTED")
             # Element is obscured, perhaps by an overlay
             try:
                 # "Refresh? Yes/No" overlay: click "No".
+                logger.info("click(): ATTEMPTING REFRESH")
                 WebDriverWait(driver, 1).until(
                     EC.presence_of_element_located(
                         (By.XPATH, '//div[contains(concat(" ", normalize-space(@class), " "), " ui-dialog ")][//span[text()="Refresh?"]]//button[text()="No"]')
                     )
                 ).click()
             except TimeoutException:
+                logger.error("click(): TIMEOUT EXCEPTION WHILE ATTEMPTING REFRESH")
                 # Another overlay, perhaps "Loading, please wait..."
                 #print("Retrying click...")
                 pass
@@ -91,14 +113,20 @@ def click_button_open_window(driver, button):
                 lambda: button.click()
             ))
         except (TimeoutException, ElementClickInterceptedException) as e:
+            if isinstance(e, TimeoutException):
+                logger.error("click_button_open_window(): TIMEOUT EXCEPTION")
+            else:
+                logger.error("click_button_open_window(): ELEMENT CLICK INTERCEPTED EXCEPTION")
             # Click may have failed if the button was outside the viewport,
             # or if another UI element such as a scrollbar had obscured it.
             # Try to scroll the button into a better position.
             try:
                 button.send_keys('\n', Keys.DOWN)
             except ElementNotInteractableException:
+                logger.error("click_button_open_window(): AFTER SCROLLING, ELEMENT NOT INTERACTABLE EXCEPTION")
                 pass
         except (ElementNotInteractableException) as e:
+            logger.error("click_button_open_window(): ELEMENT NOT INTERACTABLE EXCEPTION")
             #print(e)
             pass
 
@@ -123,6 +151,7 @@ class window_opened(object):
             return new_windows.pop()
 
 def save(driver, dest_file, url=None, mp_pool=None):
+    logger.info("save(): SAVING DATA TO dest_file {}".format(dest_file))
     """
     Save the URL (or the URL of the current browser window) to the given
     destination file.  if a multiprocessing pool is specified, the downloading
@@ -132,14 +161,19 @@ def save(driver, dest_file, url=None, mp_pool=None):
         '{0}={1}'.format(cookie['name'], cookie['value'])
         for cookie in driver.get_cookies()
     )
+    logger.info("save(): dest_file = {}, url = {}, current_url = {} cookie_string = {}"
+                .format(dest_file, url, driver.current_url, cookie_string))
     download_args = (
         dest_file,
         url if url is not None else driver.current_url,
         [('Cookie', cookie_string)],
     )
+    logger.info("save(): download_args = {}".format(download_args))
     if mp_pool:
+        logger.info("save(): DOWNLOADING ASYNC")
         mp_pool.apply_async(download, download_args)
     else:
+        logger.info("save(): DOWNLOADING")
         download(*download_args)
 
 def navigate_to_first_app(driver):
@@ -150,17 +184,23 @@ def navigate_to_first_app(driver):
     driver.get('https://evision.as.it.ubc.ca/')
     print("Please log in to eVision, then bring up evaluation folder and open the first application of interest.")
     while True:
+        logger.info("navigate_to_first_app(): WAITING FOR LOGIN")
         app_window = WebDriverWait(driver, float('inf')).until(window_opened())
+        logger.info("navigate_to_first_app(): WINDOW OPENED")
         driver.switch_to.window(app_window)
+        logger.info("navigate_to_first_app(): SWITCHED TO WINDOW {}".format(driver.current_url))
         try:
             click(driver, By.LINK_TEXT, "GPO")
             return app_window
         except TimeoutException:
+            logger.error("navigate_to_first_app(): TIMEOUT WHILE TRYING TO CLICK LINK \"GPO\"")
             pass        # This wasn't the window we were looking for?
 
 
 def request_pdf(driver):
+    logger.info("request_pdf(): STARTING...")
     app_window = driver.current_window_handle
+    logger.info("request_pdf(): app_window = {}".format(app_window))
     while True:
         try:
             click(driver, By.LINK_TEXT, "GPO")
@@ -170,53 +210,91 @@ def request_pdf(driver):
             pdf_button = WebDriverWait(driver, 90).until(EC.presence_of_element_located(
                 (By.XPATH, '//input[@type="button"][@value="Manage Applicant PDF"]')
             ))
+            logger.info("request_pdf(): pdf_button = {}".format(pdf_button))
             pdf_window = click_button_open_window(driver, pdf_button)
-            for _ in range(5):
+            logger.info("request_pdf(): pdf_window = {}".format(pdf_window))
+            logger.info("request_pdf(): SEARCHING FOR BUTTON WITH TEXT 'No'...")
+            for i in range(5):
+                logger.info("request_pdf(): ITERATION {}".format(i))
                 for button in driver.find_elements(By.XPATH, '//button'):
+                    logger.info("request_pdf(): button = {}".format(button))
                     if button.text == "No":
+                        logger.info("request_pdf(): CLICKING BUTTON")
                         button.click()
                         break
             driver.switch_to.window(pdf_window)
+            logger.info("request_pdf(): SWITCHING TO WINDOW pdf_window = {}")
             break
+        except TimeoutException:
+            logger.error("request_pdf(): TIMEOUT EXCEPTION WHILE LOCATING pdf_button")
         except StaleElementReferenceException:
+            logger.error("request_pdf(): STALE ELEMENT REFERENCE EXCEPTION")
             pass
-    continue_button = WebDriverWait(driver, 90).until(EC.presence_of_element_located(
-        (By.XPATH, '//input[@value="CONTINUE"]')
-    ))
+    logger.info("request_pdf(): LOCATING BUTTON LABELLED 'CONTINUE'...")
+    try:
+        continue_button = WebDriverWait(driver, 90).until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@value="CONTINUE"]')
+        ))
+    except TimeoutException:
+        logger.error("request_pdf(): TIMEOUT EXCEPTION WHILE LOCATING continue_button")
     for label in driver.find_elements(By.XPATH, '//label'):
+        logger.info("request_pdf(): ITERATING THROUGH LABEL {}".format(label))
         if any(bad in label.text for bad in ["Language Proficiency", "GRE"]):
+            logger.info("request_pdf(): CLICKING LABEL CONTAINING {}".format(["Language Proficiency", "GRE"]))
             label.click()
     continue_button.click()
-    WebDriverWait(driver, 120).until(EC.presence_of_element_located(
-        (By.XPATH, '//input[@value="BACK"]')
-    ))
+    logger.info("request_pdf(): LOCATING BUTTON LABELLED 'BACK'...")
+    try:
+        WebDriverWait(driver, 120).until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@value="BACK"]')
+        ))
+    except TimeoutException:
+        logger.error("request_pdf(): TIMEOUT EXCEPTION WHILE LOCATING 'BACK' BUTTON")
+    logger.info("request_pdf(): CLICK BUTTON LABELLED 'CONTINUE'")
     driver.find_element(By.XPATH, '//input[@type="submit"][@value="CONTINUE"]').click()
     # eVision bug (INC1040643): PDF merge may fail, in which case you'll see
     # "Please to download a copy of the document" instead of "Please
     # _click_here_ to download a copy of the document".
     for _ in range(120):
         try:
-            if any("Please to download" in div.text or
-                   "There has been a processing error" in div.text
-                   for div in driver.find_elements(By.TAG_NAME, 'div')):
-                pdf_url = None
-                break
+            # if any("Please to download" in div.text or
+            #
+            #        "There has been a processing error" in div.text
+            #
+            #        for div in driver.find_elements(By.TAG_NAME, 'div')):
+            for div in driver.find_elements(By.TAG_NAME, 'div'):
+                if "Please to download" in div.text or "There has been a processing error" in div.text:
+                    pdf_url = None
+                    logger.warning("request_pdf(): PROCESSING ERROR AT ELEMENT {}".format(div))
+                    break
+            logger.info("request_pdf(): LOCATING PDF URL FROM HREF...")
             pdf_url = WebDriverWait(driver, 1).until(EC.presence_of_element_located(
                 (By.LINK_TEXT, "click here")
             )).get_attribute('href')
             break
-        except (StaleElementReferenceException, TimeoutException):
+        except (StaleElementReferenceException, TimeoutException) as e:
+            if isinstance(e, TimeoutException):
+                logger.error("request_pdf(): TIMEOUT EXCEPTION WHILE GETTING pdf_url")
+            else:
+                logger.error("request_pdf(): STALE ELEMENT REFERENCE EXCEPTION WHILE GETTING pdf_url")
             pass        # Try again
+    logger.info("request_pdf(): CLICKING ELEMENT LABELLED 'EXIT'")
     click(driver, By.XPATH, '//input[@value="EXIT"]')
+    logger.info("request_pdf(): SWITCHING TO WINDOW {}".format(app_window))
     driver.switch_to.window(app_window)
     return pdf_url
 
 
 def extract_app_identity(driver):
+    logger.info("extract_app_identity(): STARTING...")
     def extract_table_text(label):
         # TODO: XPATH-escape the label
+        logger.info("extract_table_text(): EXTRACT DATA AT LABEL {}".format(label))
         td = driver.find_element(By.XPATH, '//td[strong[text()="' + label + '"]]')
-        return td.text.replace(label, '', 1).strip()
+        td_formatted = td.text.replace(label, '', 1).strip()
+        logger.info("extract_table_text(): FOUND {}, RETURNING FORMATTED DATA {}"
+                    .format(td, td_formatted))
+        return td_formatted
     # Want "Application Details" link, but By.LINK_TEXT fails when there is a space
     click(driver, By.PARTIAL_LINK_TEXT, "Details")
     # Want "Personal Details" link, but By.LINK_TEXT fails when there is a space
@@ -229,17 +307,22 @@ def extract_app_identity(driver):
     return surname, preferred_name, student_number
 
 def process_apps(driver, dest_dir):
+    logger.info("process_apps(): STARTING...")
     while True:
         try:
             surname, preferred_name, student_number = extract_app_identity(driver)
         except TimeoutException:
             # Who knows what went wrong!  Try clicking "Previous Applicant", then
             # "Next Applicant" and hopefully we'll get another chance.
+            logger.error("process_apps(): TIMEOUT EXCEPTION, CLICKING 'Previous' THEN 'Next'")
             print("Web application did not behave as expected.  Clicking "
                 """"Previous Applicant", then "Next Applicant" to try again.""")
             click(driver, By.PARTIAL_LINK_TEXT, "Previous")
             click(driver, By.PARTIAL_LINK_TEXT, "Next")
+            logger.info("process_apps(): CLICKED 'Previous' THEN 'Next', CONTINUING...")
             continue
+        logger.info("process_apps(): surname = {}, preferred_name = {}, student_number = {}"
+                    .format(surname, preferred_name, student_number))
         dest_file = os.path.join(
             dest_dir, u'{0}, {1} ({2}).pdf'.format(
                 ' ' + surname if surname.startswith('.') else surname,
@@ -247,26 +330,33 @@ def process_apps(driver, dest_dir):
                 student_number
             )
         )
+        logger.info("process_apps(): PATH TO dest_file = {}".format(dest_file))
         pdf_url = request_pdf(driver)
+        logger.info("process_apps(): pdf_url = {}".format(pdf_url))
         yield pdf_url, dest_file
 
         next_buttons = driver.find_elements(
             By.XPATH, '//input[@value="Next Applicant"][not(@disabled)]'
         )
+        logger.info("process_apps(): next_buttons = {}".format(next_buttons))
         if not next_buttons:
+            logger.warning("process_apps(): NO ELEMENT FOUND FOR next_buttons")
             break
         next_buttons[0].click()
 
 
 def parse_args(args):
+    logger.info("parse_args(): START PARSING...")
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('dest_dir',
         help='Directory to which PDFs should be saved')
     parser.add_argument('--webdriver-log', metavar='PATH', default=os.devnull,
         help='Webdriver log file (such as geckodriver.log)')
     args = parser.parse_args(args)
+    logger.info("parse_args(): INPUTTED DATA: dest_dir = {}, webdriver-log = {}".format(args.dest_dir, args.webdriver_log))
     if not os.path.isdir(args.dest_dir):
         print("Directory '{0}' does not exist".format(args.dest_dir), file=sys.stderr)
+        logger.error("parse_args(): DIRECTORY {} DOES NOT EXIST".format(args.dest_dir))
         return None
     return {
         'dest_dir': args.dest_dir,
@@ -281,16 +371,21 @@ def main(*argv):
 
     download_pool = None #Pool(2)
     driver = webdriver.Firefox(service_log_path=args.pop('webdriver_log'))
+    logger.info("DRIVER OBJECT: {}".format(driver))
     try:
         navigate_to_first_app(driver)
         for pdf_url, dest_file in process_apps(driver, args['dest_dir']):
             save(driver, dest_file, pdf_url or '', download_pool)
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, TimeoutException) as e:
+        if isinstance(e, TimeoutException):
+            logger.error("TIMEOUT EXCEPTION FROM navigate_to_first_app()")
         pass
     print('Finishing downloads...')
     if download_pool is not None:
+        logger.info("CLEARING download_pool...")
         download_pool.close()
         download_pool.join()
+    logger.info("FINISHED")
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
